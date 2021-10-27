69a864f7c7785d99d36d9e81b73761dfe955861f
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-27 15:04:06 +0000

    iterative

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 var climbStairs = function(n) {
   if (n===0) return 1
   if (n<=3) return n
+  const newArray = new Array(n+1).fill(1)
+  for (let i=2; i<=n; i++){
+    newArray[i] = newArray[i-1] + newArray[i-2]
+    // console.log(newArray)
+  }
+  return newArray[n]};
 }
 
 /*
